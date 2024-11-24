f9f7ec332d7f7ed587cc2ab7cc9c328ca64a9aaf
Dylan Dang <dvdang@berkeley.edu>
2024-11-24 13:45:46 -0800

    fixed merge conflict

---
+++ screens/ProfileScreen/ProfileScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from './ProfileScreen.styles';
+
+
+const ProfileScreen = () => {
+    return (
+        <View style={styles.container}>
+            
+        </View>
+
+)}
+
+
+export default ProfileScreen;
