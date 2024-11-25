aaf8e960f70e9ff0a981b20750b94db65c1530eb
valerie <valerie_eng@berkeley.edu>
2024-11-24 20:25:19 -0800

    undid wrong merge

--- navigation/MainStack.js
+++ navigation/MainStack.js
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ProfileScreen from '../screens/ProfileScreen/ProfileScreen';
 import WelcomeScreen from '../screens/Welcome/WelcomeScreen';
 import LoginScreen from '../screens/Login/LoginScreen';
+import ImportContactsScreen from '../screens/ImportContacts/ImportContactsScreen';
+import ConfigContactsScreen from '../screens/ConfigContactsScreen/ConfigContactsScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -25,6 +27,14 @@ const MainStack = () => {
       <Stack.Screen 
         name="Profile" 
         component={ProfileScreen} 
+      />
+      <Stack.Screen 
+        name="ImportContacts" 
+        component={ImportContactsScreen} 
+      />
+      <Stack.Screen 
+        name="ConfigContacts" 
+        component={ConfigContactsScreen} 
       />
       
   </Stack.Navigator>
