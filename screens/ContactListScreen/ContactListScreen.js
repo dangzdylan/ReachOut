8b7129307e302053fdbba4f5db6eeb677cdffefa
Dylan Dang <dvdang@berkeley.edu>
2024-11-30 17:21:30 -0800

    finished ContactListScreen routing and data retrieval

--- screens/ContactListScreen/ContactListScreen.js
+++ screens/ContactListScreen/ContactListScreen.js
@@ -3,44 +3,59 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import styles from './ContactListScreen.styles';
-import { addDoc, collection } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs} from "firebase/firestore";
 import { db } from '../../firebaseConfig';
 
-export default function ContactListScreen({ navigation }) {
- const contacts = [
-   { id: '1', name: 'Valerie Eng' },
-   { id: '2', name: 'Dylan Smith' },
-   { id: '3', name: 'Arnav Johnson' },
-   { id: '4', name: 'Renata Camargo' },
-   { id: '5', name: 'Emily Martinez' },
-   { id: '6', name: 'Selena Gomez' },
-   { id: '7', name: 'Jessica Garcia' },
- ];
+export default function ContactListScreen({ navigation, ...props}) {
+  /*
+  ASSUMING props.userId will give me the user's ID
+  */
+  const [contactList, setContactList] = useState([]);
+  const userId = props.userId || "userId1";
 
- const renderContact = ({ item }) => (
-   <View style={styles.contactItem}>
-     <View style={styles.circle} />
-     <Text style={styles.contactName}>{item.name}</Text>
-     <Ionicons name="checkmark-circle-outline" size={24} color="black" />
-   </View>
- );
+  //Grab contacts of current user
+  useEffect(() => {
+    const getData = async () => {
+      const colRef = collection(db, "users", userId, "contacts");
+      const colSnap = await getDocs(colRef);
+      let newContacts = [];
+      colSnap.forEach((doc) => {
+        newContacts.push([doc.id, doc.data()]);
+      });
 
+      setContactList(newContacts);
+    };
 
- return (
-   <View style={styles.container}>
-     <Text style={styles.header}>Contacts</Text>
-     <FlatList
-       data={contacts}
-       keyExtractor={(item) => item.id}
-       renderItem={renderContact}
-       contentContainerStyle={styles.list}
-     />
-     <TouchableOpacity style={styles.backButton} onPress={() => {navigation.navigate("HomeScreen")}}>
-       <Ionicons name="arrow-back" size={24} color="gray" />
-     </TouchableOpacity>
-     <StatusBar style="auto" />
-   </View>
- );
+    getData();
+  }, [userId])
+
+  //Subcomponent for each contact
+  const renderContact = ({ item }) => (
+    <View style={styles.contactItem}>
+      <Ionicons name="person-circle-outline" size={30} />
+      <Text style={styles.contactName}>{"  " + item[1].name}</Text>
+      <TouchableOpacity onPress={() => { navigation.navigate('Profile', { userId: userId, contactId: item[0] }) }}>
+        <Ionicons name="information-circle-outline" size={30} />
+      </TouchableOpacity>
+    </View>
+  );
+
+  //Display contacts
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Contacts</Text>
+      <FlatList
+        data={contactList}
+        keyExtractor={(item) => item[0]}
+        renderItem={renderContact}
+        contentContainerStyle={styles.list}
+      />
+      <TouchableOpacity style={styles.backButton} onPress={() => {navigation.navigate("HomeScreen")}}>
+        <Ionicons name="arrow-back" size={24} color="gray" />
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 
