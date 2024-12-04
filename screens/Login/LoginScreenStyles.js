2f7875fa0b0d56e0385cd52f6b43920a41d4fe23
Arnav Khinvasara <arnavkhinvasara1@gmail.com>
2024-12-03 23:59:26 -0800

    working on auth

--- screens/Login/LoginScreenStyles.js
+++ screens/Login/LoginScreenStyles.js
@@ -3,35 +3,45 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF', // Light yellow background color
+    backgroundColor: '#FFFFFF', // White background
   },
   content: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'center', // Center vertically
+    alignItems: 'center', // Center horizontally
     paddingHorizontal: 20,
   },
+  titleText: {
+    fontSize: 40,
+    marginBottom: 15
+  },
   buttonContainer: {
-    position: 'absolute',
-    left: 20,
-    right: 20,
-    bottom: '40%',
-    gap: 10,
+    width: '100%',
+    marginTop: 20, // Space between the last input and the button
   },
   button: {
     backgroundColor: 'white',
     paddingVertical: 12,
-    paddingHorizontal: 20,
     borderRadius: 25,
     borderWidth: 1,
     borderColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%', // Full width of the container
   },
   buttonText: {
     fontSize: 16,
     color: '#000000',
     fontWeight: '400',
   },
-});
-
-export default styles;
+  input: {
+    width: '100%', // Full width of the container
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    backgroundColor: '#f9f9f9', // Sli
+  }
+})
